3d9f438f8e4361c065f25514bc5c3ad41d92522a
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-29 13:21:16 -0400

    Code has been commented

--- download_avatars.js
+++ download_avatars.js
@@ -15,6 +15,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
       Authorization: authToken.GITHUB_TOKEN
     }
   };
+  //Create new get request at the specified url endpoint with the written options
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
@@ -28,29 +29,32 @@ function downloadImageByURL(url, filePath) {
       Authorization: authToken.GITHUB_TOKEN
     }
   };
+  //Make a get request at specified endpoint, downloads each image specifed in previous response
   request(options, (err, res) => {
     if (err) throw err;
-    try {
-      fs.mkdir("./avatars");
-    } catch (e) {
-      if (e.code == "EEXIST") {
-      }
-    }
-  }).pipe(fs.createWriteStream(filePath));
+    //Attempt to make directory
+    fs.mkdir("./avatars", error => {
+      fs.createWriteStream(filePath);
+    });
+  });
 }
 
 //Function to be executed on program start
 (function() {
   console.log("Welcome to the GitHub Avatar Downloader!");
+  //Slice first two identifiers off of command line arguments to remain with just the user specified arguments
   const args = process.argv.slice(2);
+  //If there is the correct number of arguments continue with the operation
   if (args.length == 2) {
     getRepoContributors(args[0], args[1], (err, result) => {
       console.log("Errors:", err);
       for (let el of result) {
+        //Download each image based on the provided url, and place it in an dynamically created directory and file name
         downloadImageByURL(el["avatar_url"], "avatars/" + el["login"] + ".jpg");
       }
     });
   } else {
+    //If arguments are somehow incorrect throw an error explaining what was expected
     throw "Incorrect Number of Arguments!\nThe arguments expected are <owner> <repo>\nOrder is important";
   }
 })();
