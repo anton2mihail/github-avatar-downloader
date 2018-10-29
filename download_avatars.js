9ce4471aff7332c1c4dd243ab8c5bdc144f198b0
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-29 12:13:07 -0400

    Added authentication sucessfully

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,30 @@
 const request = require("request");
-
+const authToken = require("./secrets");
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+  var options = {
+    url:
+      "https://api.github.com/repos/" +
+      repoOwner +
+      "/" +
+      repoName +
+      "/contributors",
+    headers: {
+      "User-Agent": "request",
+      Authorization: authToken.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  for (let el of result) {
+    console.log(el["avatar_url"]);
+  }
 });
