8accf5c0387fe0829c72282ab97ef966f44a8765
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-29 12:55:21 -0400

    File formatting completed, and the creation of auto-invoke function

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,7 @@
 const request = require("request");
 const authToken = require("./secrets");
-console.log("Welcome to the GitHub Avatar Downloader!");
-
+const fs = require("fs");
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
   var options = {
     url:
       "https://api.github.com/repos/" +
@@ -16,19 +14,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
       Authorization: authToken.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
 }
-
-getRepoContributors("jquery", "jquery", (err, result) => {
-  console.log("Errors:", err);
-  for (let el of result) {
-    downloadImageByURL(el["avatar_url"], "avatars/" + el["login"] + ".jpg");
-  }
-});
-const fs = require("fs");
 function downloadImageByURL(url, filePath) {
   let options = {
     url: url,
@@ -39,5 +28,27 @@ function downloadImageByURL(url, filePath) {
   };
   request(options, (err, res) => {
     if (err) throw err;
+    try {
+      fs.mkdir("./avatars");
+    } catch (e) {
+      if (e.code == "EEXIST") {
+      }
+    }
   }).pipe(fs.createWriteStream(filePath));
 }
+
+//Function to be executed on program start
+(function() {
+  console.log("Welcome to the GitHub Avatar Downloader!");
+  const args = process.argv.slice(2);
+  if (args.length == 2) {
+    getRepoContributors(args[0], args[1], (err, result) => {
+      console.log("Errors:", err);
+      for (let el of result) {
+        downloadImageByURL(el["avatar_url"], "avatars/" + el["login"] + ".jpg");
+      }
+    });
+  } else {
+    throw "Incorrect Number of Arguments!\nThe arguments expected are <owner> <repo>\nOrder is important";
+  }
+})();
